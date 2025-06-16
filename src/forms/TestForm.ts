d45f661ea3093044754c1d918254d1c0dd8722f7
ahoeffner <alex@hoeffner.net>
2025-06-16 17:01:48 +0200

    .

--- src/forms/TestForm.ts
+++ src/forms/TestForm.ts
@@ -1,7 +1,10 @@
-import { BusinessEvent, BusinessEventListener, BusinessEvents, Form, FormEventFilter } from "futureforms";
+import { Destination, BusinessEvent, BusinessEventListener, Form, FormEventFilter, Field } from "futureforms";
 
 export class TestForm extends Form implements BusinessEventListener
 {
+	public id:number = TestForm.id$++;
+
+	public static id$:number = 0;
 	public static instances:TestForm[] = [];
 
 
@@ -11,14 +14,31 @@ export class TestForm extends Form implements BusinessEventListener
 		TestForm.instances.push(this);
       console.log("TestForm created")
 
-		let test:FormEventFilter = new FormEventFilter("input",this,"block","field");
-		BusinessEvents.addListener(this,test);
+		let dest:Destination = {component: this, function: null};
+
+		let test1:FormEventFilter = new FormEventFilter(this,"input");
+		let test2:FormEventFilter = new FormEventFilter(this,"input","CountryDS","country_name");
+
+		//this.addTrigger(this,test1);
+		//this.addTrigger(this.field,test2);
+		this.addTrigger(dest.function = this.handleBusinessEvent,test1);
    }
 
 
 	public async handleBusinessEvent(event:BusinessEvent) : Promise<boolean>
 	{
-      console.log("Event handled:",event);
+		let row:number 	= event.properties.get(Field.ROW);
+		let value:any 		= event.properties.get(Field.VALUE);
+		let field:string 	= event.properties.get(Field.FIELD);
+
+      console.log("general::Event handled: "+event.type+" "+field+"["+row+"] = "+value);
+		return(true);
+   }
+
+
+	public async field(event:BusinessEvent) : Promise<boolean>
+	{
+      console.log("field::Event handled: "+event.type+" "+this.id);
 		return(true);
    }
 }
